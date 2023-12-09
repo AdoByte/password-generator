7d8c702af3a0f3cb2a9fba8dbe3266f6aafd6e37
Adobyte <adobyte77@gmail.com>
2023-12-09 18:18:38 +0100

    Request user inputs to determine password features
    - Password features like length, uppercase character inclusion are
      determined by user input
    - Number of passwords to be generated is stated

--- password_generator.py
+++ password_generator.py
@@ -18,3 +18,42 @@ def generate_password(length, uppercase: bool, symbol: bool):
         new_password += combination[secrets.randbelow(len(combination))]        # len(combination) is the upper limit
         # for randbelow
     return new_password
+
+
+def contains_uppercase(password):
+
+    for char in password:
+        if char.isupper():
+            return True
+    return False
+
+
+def contains_symbols(password):
+
+    for char in password:
+        if char in string.punctuation:
+            return True
+    return False
+
+
+if __name__ == '__main__':
+
+    num_password = int(input("How many passwords do you want to generate?  "))
+    length = int(input("What is the length of the passwords?  "))
+    uppercase = input("Can contain uppercase? y/n   ")
+    symbols = input("Can contain symbols? y/n  ")
+
+    if uppercase.lower() == 'y':
+        uppercase = True
+    else:
+        uppercase = False
+
+    if symbols.lower() == 'y':
+        symbols = True
+    else:
+        symbols = False
+    print()
+    for i in range(1, num_password+1):
+        new_pass = generate_password(length, uppercase, symbols)
+        new_pass_check = f"U: {contains_uppercase(new_pass)}, S: {contains_symbols(new_pass)}"
+        print(f"{i} ->  {new_pass}   {new_pass_check}")
