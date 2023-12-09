5beb367dc33edb7c430c969b7d4d2aa0181000ae
Adobyte <adobyte77@gmail.com>
2023-12-09 20:28:56 +0100

    Rename variables to match scope

--- password_generator.py
+++ password_generator.py
@@ -2,19 +2,19 @@ import string
 import secrets
 
 
-def generate_password(length, uppercase: bool, symbol: bool):
+def generate_password(length, uppercase: bool, symbols: bool):
 
     combination = string.ascii_lowercase + string.digits
 
     if uppercase:
         combination += string.ascii_uppercase
 
-    if symbol:
+    if symbols:
         combination += string.punctuation
 
     new_password = ''
 
-    for i in range(length):         # length of password to be generated
+    for r in range(length):         # length of password to be generated
         new_password += combination[secrets.randbelow(len(combination))]        # len(combination) is the upper limit
         # for randbelow
     return new_password
@@ -39,21 +39,21 @@ def contains_symbols(password):
 if __name__ == '__main__':
 
     num_password = int(input("How many passwords do you want to generate?  "))
-    length = int(input("What is the length of the passwords?  "))
-    uppercase = input("Can contain uppercase? y/n   ")
-    symbols = input("Can contain symbols? y/n  ")
+    pass_length = int(input("What is the length of the passwords?  "))
+    allow_uppercase = input("Can contain uppercase? y/n   ")
+    allow_symbols = input("Can contain symbols? y/n  ")
 
-    if uppercase.lower() == 'y':
-        uppercase = True
+    if allow_uppercase.lower() == 'y':
+        allow_uppercase = True
     else:
-        uppercase = False
+        allow_uppercase = False
 
-    if symbols.lower() == 'y':
-        symbols = True
+    if allow_symbols.lower() == 'y':
+        allow_symbols = True
     else:
-        symbols = False
+        allow_symbols = False
     print()
     for i in range(1, num_password+1):
-        new_pass = generate_password(length, uppercase, symbols)
+        new_pass = generate_password(pass_length, allow_uppercase, allow_symbols)
         new_pass_check = f"U: {contains_uppercase(new_pass)}, S: {contains_symbols(new_pass)}"
         print(f"{i} ->  {new_pass}   {new_pass_check}")
